7cde0738b31650671e359fdc0e67f45692aa4bb7
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2012-02-01 02:50:56 +0100

    1. Implementace fetche stránky a hlaviček + výpis.

---
+++ src/tk/ludva/restfulchecker/APINotAccesibleException.java
@@ -0,0 +1,14 @@
+package tk.ludva.restfulchecker;
+
+public class APINotAccesibleException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public APINotAccesibleException(String message) {
+		
+	}
+
+}
--- src/tk/ludva/restfulchecker/APIcheckerController.java
+++ src/tk/ludva/restfulchecker/APIcheckerController.java
@@ -1,5 +1,16 @@
 package tk.ludva.restfulchecker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +22,52 @@ public class APIcheckerController {
 
 	@RequestMapping(value="/checkapi/", method=RequestMethod.POST)
 	@ResponseBody
-	public String checkAPI(@RequestParam String apiEntryURL) {
+	public String checkAPI(@RequestParam String apiEntryURL) throws APINotAccesibleException {
 		StringBuilder response = new StringBuilder();
-		response.append("Vstupni bod API je: ").append(apiEntryURL).append('\n');
-		
-		
+		response.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n  <head>\n    <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n    <title>RESTfulChecker</title>\n  </head>\n  <body>");
+		response.append("Vstupni bod API je: ").append(apiEntryURL).append("<br>");
+		URL remoteUrl;
+		try {
+			remoteUrl = new URL(apiEntryURL);
+		} catch (MalformedURLException e) {
+			throw new APINotAccesibleException("MalformedURLException: "+apiEntryURL);
+		}
+		URLConnection remoteApiConnection;
+		try {
+			remoteApiConnection = remoteUrl.openConnection();
+		} catch (IOException e) {
+			throw new APINotAccesibleException("IOException: "+apiEntryURL);
+		}
+		Map<String, List<String>> remoteHeaderFields = remoteApiConnection.getHeaderFields();
+		Set<String> remoteHeadersKeys = remoteHeaderFields.keySet();
+		response.append("<table border=\"1\">");
+		for (Iterator<String> remoteHeadersKeysIterator = remoteHeadersKeys.iterator(); 
+				remoteHeadersKeysIterator.hasNext();) {
+			response.append("<tr>");
+			String headerKey = (String) remoteHeadersKeysIterator.next();
+			response.append("<th>"+headerKey+" : </th>");
+			List<String> remoteHeaderValues = remoteHeaderFields.get(headerKey);
+			for (Iterator<String> remoteHeaderValuesIterator = remoteHeaderValues.iterator(); 
+					remoteHeaderValuesIterator.hasNext();) {
+				String remoteHeaderValue = (String) remoteHeaderValuesIterator.next();
+				response.append("<td>"+remoteHeaderValue+"</td>");
+			}
+			response.append("</tr>\n");
+		}
+		response.append("</table>");
+		response.append("\n");
+		try {
+			StringBuilder soubor = new StringBuilder();
+			String radek;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(remoteUrl.openStream()));
+			while ((radek = reader.readLine()) != null) {
+				soubor.append(radek);
+			}
+			response.append("<xmp>"+soubor+"</xmp>");
+		} catch (IOException e) {
+			throw new APINotAccesibleException("IOException: "+apiEntryURL);
+		}
+		response.append("</body></html>");
 		return response.toString();
 	}
 }
