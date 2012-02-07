b922949be16562c02d2eefb1251f33e81609b800
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2012-02-07 21:03:50 +0100

    Cleanup + first try of implementing JSP->Bean Map<String,String> - not working

--- src/tk/ludva/restfulchecker/RemoteResource.java
+++ src/tk/ludva/restfulchecker/RemoteResource.java
@@ -1,14 +1,26 @@
 package tk.ludva.restfulchecker;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class RemoteResource {
 	private String url="http://";
 	private String method;
-	private Map<String, String> headers;
-	private String body;
+	private Map<String, String> requestHeaders;
+	private Map<String, String> responseHeaders;
+	private String requestBody;
+	private String responseBody;
+	private String responseCode;
+	private String responseMessage;
 
 	public RemoteResource() {
+		requestHeaders = new HashMap<String, String>();
+		requestHeaders.put("hlavicka", "jeji hodnota");
+		requestHeaders.put("hlavicka2", "jeji hodnota2");
 	}
 
 	public String getUrl() {
@@ -27,24 +39,68 @@ public class RemoteResource {
 		this.method = method;
 	}
 
-	public Map<String, String> getHeaders() {
-		return headers;
+	public Map<String, String> getRequestHeaders() {
+		return requestHeaders;
 	}
 
-	public void setHeaders(Map<String, String> headers) {
-		this.headers = headers;
+	public void setRequestHeaders(Map<String, String> requestHeaders) {
+		this.requestHeaders = requestHeaders;
 	}
 
-	public String getBody() {
-		return body;
+	public Map<String, String> getResponseHeaders() {
+		return responseHeaders;
 	}
 
-	public void setBody(String body) {
-		this.body = body;
+	public void setResponseHeaders(Map<String, String> responseHeaders) {
+		this.responseHeaders = responseHeaders;
+	}
+
+	public String getRequestBody() {
+		return requestBody;
+	}
+
+	public void setRequestBody(String requestBody) {
+		this.requestBody = requestBody;
+	}
+
+	public String getResponseBody() {
+		return responseBody;
+	}
+
+	public void setResponseBody(String responseBody) {
+		this.responseBody = responseBody;
+	}
+
+	public String getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(String responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}	
+	
+	public Set<String> getRequestHeadersKeys() {
+		return requestHeaders.keySet();
 	}
 	
+	public void setRequestHeadersKeys(List<String> requestHeadersKeys) {
+		requestHeaders = new HashMap<String, String>();
+		for (Iterator<String> iterator = requestHeadersKeys.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			requestHeaders.put(string, null);
+		}
+	}
 	
-	
-	
+	public Collection<String> getRequestHeaderValues() {
+		return requestHeaders.values();
+	}
 	
 }
--- war/checkapi.jsp
+++ war/checkapi.jsp
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="utf-8" ?>
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml"> 
   <head> 
@@ -9,7 +10,7 @@
   </head> 
   <body>
   <h1>RESTfulChecker</h1>
-    
+  <h2>Request:</h2>
     <form:form commandName="remoteResource" action="/checkapi/" >
       <table>
         <tr>
@@ -30,7 +31,22 @@
           <td><input type="submit" value="check this" /></td>
         </tr>
       </table>
+      
+      <table>
+      	<c:forEach items="${remoteResource.requestHeadersKeys}" var="neco">
+      	  <tr>
+      	    <td>
+      	      <input type="text" id="requestHeadersKeys[]" name="requestHeadersKeys[]" value="${neco}"/>
+      	    </td>
+      	    <td>
+      	      <input type="text" id="requestHeadersValues[]" name="requestHeadersValues[]" value="${remoteResource.requestHeaders[neco]}"/>
+      	    </td>
+      	  </tr>
+      	</c:forEach>
+      </table>
     </form:form>
+    
+    <h2>Response:</h2>
     
 
   </body>
